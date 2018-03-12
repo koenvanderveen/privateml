0b3fc0ff00e8acf1f445cf4374c16bae081d7d0e
ruben seggers <rubenseggers@gmail.com>
2018-03-12 14:53:07 +0100

    manage directories

--- image_analysis/pond/nn.py
+++ image_analysis/pond/nn.py
@@ -2,13 +2,12 @@ import numpy as np
 import sys
 from datetime import datetime
 from functools import reduce
-from pond.tensor import NativeTensor, PrivateEncodedTensor, PublicEncodedTensor
-from im2col import im2col_indices, col2im_indices
+from pond.tensor import NativeTensor
+from im2col.im2col import im2col_indices, col2im_indices
 import math
-import time
 
 try:
-    from image_analysis.im2col_cython import im2col_cython, col2im_cython
+    from im2col.im2col_cython import im2col_cython, col2im_cython
     cython = True
 except ImportError:
     print('run the following from the image_analysis directory to use cython:')
